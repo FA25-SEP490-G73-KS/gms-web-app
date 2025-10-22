09e376632da228a0b20d4106598c0577a7308de4
huyendthe172170-ops <huyendthe172170@fpt.edu.vn>
2025-10-22 11:01:08 +0700

    routes auth

--- app/routes.ts
+++ app/routes.ts
@@ -5,4 +5,10 @@ export default [
   route("/appointments", "routes/appointments.tsx"),
   route("/service-tickets", "routes/service-tickets.tsx"),
   route("/service-tickets-new", "routes/service-tickets.new.tsx"),
+    route("/login", "routes/login.tsx"),
+    route("/forgot-password", "routes/forgot-password.tsx"),
+    route("/otp", "routes/otp.tsx"),
+    route("/reset-password", "routes/reset-password.tsx"),
+    route("/reset-success", "routes/reset-success.tsx"),
+
 ] satisfies RouteConfig;
